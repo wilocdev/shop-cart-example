352b425a5140fdc3ac062001133979965682f7a3
wilson <wilson.ochoau@gmail.com>
2025-08-13 21:24:10 -0500

    Change Header Menu Cart Box

--- src/components/OpenCartBox.jsx
+++ src/components/OpenCartBox.jsx
@@ -6,14 +6,14 @@ export default function OpenCartBox() {
 
   return (
     <div
-      className={`right-2 top-2 items-center justify-end gap-4  p-2 rounded-3xl min-w-40 OpenCartBox ${
+      className={`top-0 items-center justify-between gap-4  py-2 px-6 w-full bg-black md:min-w-40 text-white ${
         openCart ? 'hidden' : 'fixed flex'
       }`}
     >
       <span className="text-2xl">{getTotal().toLocaleString('es-co')}</span>
       <button
         onClick={handleOpenCart}
-        className="rounded-full p-3 bg-black cursor-pointer text-white stroke-2 hover:text-black hover:bg-white transition duration-400"
+        className="rounded-full p-3 border-2 bg-black cursor-pointer text-white stroke-2 hover:text-black hover:bg-white transition duration-400"
       >
         <OpenCart />
       </button>
